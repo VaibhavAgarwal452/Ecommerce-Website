bfb4dfb794dd4824c975e75a9ad42dde56c5f50a
Vaibhav Agarwal <vaibhavagarwal78@gmail.com>
2021-04-19 20:32:22 +0530

    HomeScreen Added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './assets/bootstrap.min.css';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Ecommerce</h1>
+          <HomeScreen />
         </Container>
       </main>
 
---
+++ Frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Products from '../products';
+import Product from '../components/Product';
+
+const HomeScreen = () => {
+  return (
+    <Fragment>
+      <Container>
+        <Row>
+          {Products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default HomeScreen;
