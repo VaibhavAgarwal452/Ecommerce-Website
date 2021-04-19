9c895f285a8c5e6904952f262a9d26691b0039d6
Vaibhav Agarwal <vaibhavagarwal78@gmail.com>
2021-04-19 19:13:20 +0530

    React SetUp

---
+++ Frontend/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React, { Fragment, useState } from 'react';
+import './assets/output.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return <div>Hello</div>;
+}
+
+export default App;
