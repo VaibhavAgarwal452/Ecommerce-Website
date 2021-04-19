38bc9b369f69d179ddf68f96290ce4febfa9ddcf
Vaibhav Agarwal <vaibhavagarwal78@gmail.com>
2021-04-19 19:19:12 +0530

    Minor Changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import React, { Fragment, useState } from 'react';
 import './assets/output.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
