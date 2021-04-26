c40c9087db277be5457c2509488622cef037de6f
Vaibhav Agarwal <vaibhavagarwal78@gmail.com>
2021-04-27 04:51:04 +0530

    Minor changes

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,28 +1,33 @@
 import express from 'express';
 const router = express.Router();
 import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productControllers.js';
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+// router.get(
+//   '/',
+//   asyncHandler(async (req, res) => {
+//     const products = await Product.find({});
 
-    res.json(products);
-  })
-);
+//     res.json(products);
+//   })
+// );
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product Not Found');
-    }
-  })
-);
+// router.get(
+//   '/:id',
+//   asyncHandler(async (req, res) => {
+//     const product = await Product.findById(req.params.id);
+//     if (product) {
+//       res.json(product);
+//     } else {
+//       res.status(404);
+//       throw new Error('Product Not Found');
+//     }
+//   })
+// );
 
 export default router;
