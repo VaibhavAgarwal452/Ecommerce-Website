59a761c29de4fb75594ead984c30a0ac4aa1aead
Vaibhav Agarwal <vaibhavagarwal78@gmail.com>
2021-04-28 01:28:09 +0530

    Backend Changes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile } from '../controllers/userControllers.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userControllers.js';
 import { protect } from '../middleWare/authMiddleware.js';
 
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
+router.route('/').post(registerUser);
 
 export default router;
